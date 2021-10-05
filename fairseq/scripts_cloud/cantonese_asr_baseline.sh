f41f2963ce9cc8717ae8b9a9c0f2ab20952a6173
khleeloo <frieske@gmail.com>
2021-10-05 10:21:24 +0800

    added script for testing

--- fairseq/scripts_cloud/cantonese_asr_baseline.sh
+++ fairseq/scripts_cloud/cantonese_asr_baseline.sh
@@ -7,6 +7,7 @@ DATA_ROOT="home/rita/cantonese-asr/dataset"
 cd fairseq
 pip install --editable ./
 
+#training
 CUDA_VISIBLE_DEVICES=0
 fairseq-train ${DATA_ROOT} --train-subset cnt_asr_train_metadata --valid-subset cnt_asr_valid_metadata \
     --task speech_to_text --config-yaml config_asr.yaml  \
@@ -16,13 +17,15 @@ fairseq-train ${DATA_ROOT} --train-subset cnt_asr_train_metadata --valid-subset 
     --optimizer adam --lr 2e-3 --lr-scheduler inverse_sqrt --warmup-updates 10000 \
     --max-tokens 12000 --clip-norm 10.0 --seed 1 --update-freq 8 --save-dir ${SAVE_DIR}
     
-    
+#checkpoint averaging    
 CHECKPOINT_FILENAME=avg_last_10_checkpoint.pt
 python scripts/average_checkpoints.py --inputs ${SAVE_DIR} \
   --num-epoch-checkpoints 10 \
   --output "${SAVE_DIR}/${CHECKPOINT_FILENAME}"
+  
+#testing      
 for SUBSET in dev-clean dev-other test-clean test-other; do
-  fairseq-generate ${LS_ROOT} --config-yaml config.yaml --gen-subset ${SUBSET} \
+  fairseq-generate ${LS_ROOT} --config-yaml config_asr.yaml --gen-subset ${SUBSET} \
     --task speech_to_text --path ${SAVE_DIR}/${CHECKPOINT_FILENAME} \
     --max-tokens 50000 --beam 5 --scoring wer
 done
